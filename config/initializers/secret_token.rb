687ab09388e2530536f4191df1f33550f8c6e0d2
Robert Reynolds <robertreynolds@Avants-Mac-mini-3.local>
2014-02-27 17:12:59 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocitoff::Application.config.secret_token = 'c40a9982979f19c6b8956fcd133349ad67f604f142d2139fb6639a14890cd268a962747d8ce23c1b69e877a6eec7d3514a4568dfd766c21d3aa60da45ca0144a'
