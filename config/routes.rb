aec022c1ca735bca4aa736b6cbc026fe79d6530c
Robert Reynolds <robert.j.reynolds@gmail.com>
2014-03-04 08:45:22 -0600

    finished setting up devise, setting user model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Blocitoff::Application.routes.draw do
 
+  devise_for :users
+
   get "home/index"
 
   get "home/about"
 
-  root :to 'home#index'
-  
+  root :to =>'home#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
